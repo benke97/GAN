8f055e9a34699933a255aca4e55f5d05783f1fe0
angloth <angus.lothian@hotmail.com>
2024-01-22 22:19:04 +0100

    Add validation loss calculations

--- train.py
+++ train.py
@@ -21,23 +21,23 @@ experimental_path = Path('data') / 'experimental'
 sim_data_dict, exp_data_dict, transform_sim, transform_exp = get_data_dicts_and_transforms(mrc_path, experimental_path, apply_probe=True, only_random=False)
 
 simulated_dataset = SimulatedDataset(sim_data_dict, transform=transform_sim)
-simulated_loader = DataLoader(simulated_dataset, batch_size=1, shuffle=True)
-
 experimental_dataset = ExperimentalDataset(exp_data_dict, transform=transform_exp)
-experimental_loader = DataLoader(experimental_dataset, batch_size=1, shuffle=True)
 
 train_size_sim = int(0.8 * len(simulated_dataset))
 val_size_sim = len(simulated_dataset) - train_size_sim
+
+BATCH_SIZE = 1
+
 train_dataset_sim, val_dataset_sim = random_split(simulated_dataset, [train_size_sim, val_size_sim])
 train_dataset_exp, val_dataset_exp = random_split(experimental_dataset, [train_size_sim, val_size_sim])
-simulated_loader_train = DataLoader(train_dataset_sim, batch_size=1, shuffle=True)
-simulated_loader_val = DataLoader(val_dataset_sim, batch_size=1, shuffle=True)
-experimental_loader_train = DataLoader(train_dataset_exp, batch_size=1, shuffle=True)
-experimental_loader_val = DataLoader(val_dataset_exp, batch_size=1, shuffle=True)
+simulated_loader_train = DataLoader(train_dataset_sim, batch_size=BATCH_SIZE, shuffle=True)
+simulated_loader_val = DataLoader(val_dataset_sim, batch_size=BATCH_SIZE, shuffle=True)
+experimental_loader_train = DataLoader(train_dataset_exp, batch_size=BATCH_SIZE, shuffle=True)
+experimental_loader_val = DataLoader(val_dataset_exp, batch_size=BATCH_SIZE, shuffle=True)
 
 #plot a batch of images from both sets
-plot_batch(simulated_loader, 'Simulated Images')
-plot_batch(experimental_loader, 'Experimental Images')
+plot_batch(simulated_loader_train, 'Simulated Images')
+plot_batch(experimental_loader_train, 'Experimental Images')
 
 print(len(simulated_dataset), len(experimental_dataset))
 #plot the average histograms of the two datasets in the same plot
@@ -48,10 +48,6 @@ print(len(simulated_dataset), len(experimental_dataset))
 
 #%%
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-#G_AB = Generator().to(device)
-#G_BA = Generator().to(device)
-#D_A = Discriminator().to(device)
-#D_B = Discriminator().to(device)
 
 norm = "instance"
 G_AB = ResnetGenerator(norm, n_blocks=6, use_dropout=False).to(device)
@@ -75,8 +71,6 @@ optimizer_D_B = torch.optim.Adam(D_B.parameters(), lr=0.00025)
 
 #0.001,0.001,0.0003, 0.00001 works but discriminators are trash
 
-loader_A = experimental_loader
-loader_B = simulated_loader
 lambda_cycle = 10
 lambda_id = 0.5 * lambda_cycle
 
@@ -90,8 +84,8 @@ log_file_path = "training_log.txt"
 writer = SummaryWriter()
 
 from skimage.metrics import structural_similarity as ssim
-fixed_sample_A = next(iter(experimental_loader))[0].to(device)  # Fixed batch from domain A
-fixed_sample_B = next(iter(simulated_loader))[0].to(device)     # Fixed batch from domain B
+fixed_sample_A = next(iter(experimental_loader_val))[0].to(device)  # Fixed batch from domain A
+fixed_sample_B = next(iter(simulated_loader_val))[0].to(device)     # Fixed batch from domain B
 
 fixed_sample_A = fixed_sample_A.unsqueeze(0)
 fixed_sample_B = fixed_sample_B.unsqueeze(0)
@@ -107,9 +101,14 @@ for epoch in range(num_epochs):
     epoch_loss_D_A = 0
     epoch_loss_D_B = 0
 
-    loader_A = tqdm(experimental_loader, total=len(experimental_loader))
-    loader_B = tqdm(simulated_loader, total=len(simulated_loader))
-    for real_A, real_B in zip(loader_A, loader_B):
+    G_AB.train()
+    G_BA.train()
+    D_A.train()
+    D_B.train()
+
+    loader_A_train = tqdm(experimental_loader_train, total=len(experimental_loader_train))
+    loader_B_train = tqdm(simulated_loader_train, total=len(simulated_loader_train))
+    for real_A, real_B in zip(loader_A_train, loader_B_train):
 
         real_A = real_A.to(device)
         real_B = real_B.to(device)
@@ -188,7 +187,7 @@ for epoch in range(num_epochs):
         loss_D_B.backward()
         optimizer_D_B.step()
 
-        batch = epoch * len(loader_A) + loader_A.n
+        batch = epoch * len(loader_A_train) + loader_A_train.n
         writer.add_scalar("loss_G_AB", loss_G_AB.item(), batch)
         writer.add_scalar("loss_G_BA", loss_G_BA.item(), batch)
         writer.add_scalar("loss_D_A", loss_D_A.item(), batch)
@@ -222,10 +221,10 @@ for epoch in range(num_epochs):
             log_file.write(str(loss_dict) + "\n")
 
 
-    avg_loss_G_AB = epoch_loss_G_AB / len(loader_A)
-    avg_loss_G_BA = epoch_loss_G_BA / len(loader_A)
-    avg_loss_D_A = epoch_loss_D_A / len(loader_A)
-    avg_loss_D_B = epoch_loss_D_B / len(loader_A)
+    avg_loss_G_AB = epoch_loss_G_AB / len(loader_A_train)
+    avg_loss_G_BA = epoch_loss_G_BA / len(loader_A_train)
+    avg_loss_D_A = epoch_loss_D_A / len(loader_A_train)
+    avg_loss_D_B = epoch_loss_D_B / len(loader_A_train)
 
     losses_G_AB.append(avg_loss_G_AB)
     losses_G_BA.append(avg_loss_G_BA)
@@ -233,7 +232,119 @@ for epoch in range(num_epochs):
     losses_D_B.append(avg_loss_D_B)
 
     # Print average losses
-    print(f"Generator AB Loss: {avg_loss_G_AB:.5f}, Generator BA Loss: {avg_loss_G_BA:.5f}, Discriminator A Loss: {avg_loss_D_A:.5f}, Discriminator B Loss: {avg_loss_D_B:.5f}")
+    print(f"TRAINING: Generator AB Loss: {avg_loss_G_AB:.5f}, Generator BA Loss: {avg_loss_G_BA:.5f}, Discriminator A Loss: {avg_loss_D_A:.5f}, Discriminator B Loss: {avg_loss_D_B:.5f}")
+
+    # Calcualte validation losses
+    G_AB.eval()
+    G_BA.eval()
+    D_A.eval()
+    D_B.eval()
+
+    val_loss_G_AB = 0
+    val_loss_G_BA = 0
+    val_loss_D_A = 0
+    val_loss_D_B = 0
+    val_loss_id_A = 0
+    val_loss_id_B = 0
+    val_loss_GAN_AB = 0
+    val_loss_GAN_BA = 0
+    val_loss_cycle_A = 0
+    val_loss_cycle_B = 0
+
+    with torch.no_grad():
+        loader_A_val = tqdm(experimental_loader_val, total=len(experimental_loader_val))
+        loader_B_val = tqdm(simulated_loader_val, total=len(simulated_loader_val))
+
+        for real_A, real_B in zip(loader_A_val, loader_B_val):
+            
+            real_A = real_A.to(device)
+            real_B = real_B.to(device)
+            #add gaussian noise to real_B with mean 0 and std 0.001
+            noise = torch.randn(real_B.size(), device=device) * 0.0001
+            real_B = real_B.clone() + noise
+
+            # Identity loss
+            loss_id_A = criterion_identity(G_BA(real_A), real_A)
+            loss_id_B = criterion_identity(G_AB(real_B), real_B)
+
+            # GAN loss
+            fake_B = G_AB(real_A)
+
+            discriminator_output = D_B(fake_B)
+            target_tensor = torch.ones_like(discriminator_output).to(device)  
+            loss_GAN_AB = criterion_GAN(discriminator_output, target_tensor)
+
+            fake_A = G_BA(real_B)
+
+            discriminator_output = D_A(fake_A)
+            target_tensor = torch.ones_like(discriminator_output).to(device)
+            loss_GAN_BA = criterion_GAN(discriminator_output, target_tensor)
+
+            # Cycle loss
+            recovered_A = G_BA(fake_B)
+            loss_cycle_A = criterion_cycle(recovered_A, real_A)
+            recovered_B = G_AB(fake_A)
+            loss_cycle_B = criterion_cycle(recovered_B, real_B)
+
+            # Total loss
+            #print(loss_GAN_AB.item(), loss_GAN_BA.item(), loss_cycle_A.item(), loss_cycle_B.item(), loss_id_A.item(), loss_id_B.item(), lambda_cycle*(loss_cycle_A.item() + loss_cycle_B.item()), lambda_id*(loss_id_A.item() + loss_id_B.item()))
+            loss_G_AB = loss_GAN_AB + lambda_cycle * loss_cycle_B + lambda_id * loss_id_B
+            
+            loss_G_BA = loss_GAN_BA + lambda_cycle * loss_cycle_A + lambda_id * loss_id_A
+
+            # Discriminator A
+            real_A_output = D_A(real_A)
+            target_real = torch.ones_like(real_A_output).to(device)
+            loss_real = criterion_GAN(real_A_output, target_real)
+
+            fake_A_output = D_A(fake_A.detach())
+            target_fake = torch.zeros_like(fake_A_output).to(device)
+            loss_fake = criterion_GAN(fake_A_output, target_fake)
+
+            loss_D_A = (loss_real + loss_fake) / 2
+
+            # Discriminator B
+            real_B_output = D_B(real_B)
+            target_real_B = torch.ones_like(real_B_output).to(device)
+            loss_real_B = criterion_GAN(real_B_output, target_real_B)
+
+            fake_B_output = D_B(fake_B.detach())
+            target_fake_B = torch.zeros_like(fake_B_output).to(device) 
+            loss_fake_B = criterion_GAN(fake_B_output, target_fake_B)
+
+            # Combine the losses
+            loss_D_B = (loss_real_B + loss_fake_B) / 2
+
+            val_loss_G_AB += loss_G_AB.item()
+            val_loss_G_BA += loss_G_BA.item()
+            val_loss_D_A += loss_D_A.item()
+            val_loss_D_B += loss_D_B.item()
+            val_loss_id_A += loss_id_A.item()
+            val_loss_id_B += loss_id_B.item()
+            val_loss_GAN_AB += loss_GAN_AB.item()
+            val_loss_GAN_BA += loss_GAN_BA.item()
+            val_loss_cycle_A += loss_cycle_A.item()
+            val_loss_cycle_B += loss_cycle_B.item()
+
+        avg_val_loss_G_AB = val_loss_G_AB / val_size_sim
+        avg_val_loss_G_BA = val_loss_G_BA / val_size_sim
+        avg_val_loss_D_A = val_loss_D_A / val_size_sim
+        avg_val_loss_D_B = val_loss_D_B / val_size_sim
+        
+        print(f"VALIDATION: Generator AB Loss: {avg_val_loss_G_AB:.5f}, Generator BA Loss: {avg_val_loss_G_BA:.5f}, Discriminator A Loss: {avg_val_loss_D_A:.5f}, Discriminator B Loss: {avg_val_loss_D_B:.5f}")
+
+        iters = epoch * len(loader_A_train)
+        writer.add_scalar("val_loss_G_AB", avg_val_loss_G_AB, iters)
+        writer.add_scalar("val_loss_G_BA", avg_val_loss_G_BA, iters)
+        writer.add_scalar("val_loss_D_A", avg_val_loss_D_A, iters)
+        writer.add_scalar("val_loss_D_B", avg_val_loss_D_B, iters)
+        writer.add_scalar("val_loss_id_A", val_loss_id_A / val_size_sim, iters)
+        writer.add_scalar("val_loss_id_B", val_loss_id_B / val_size_sim, iters)
+        writer.add_scalar("val_loss_GAN_AB", val_loss_GAN_AB / val_size_sim, iters)
+        writer.add_scalar("val_loss_GAN_BA", val_loss_GAN_BA / val_size_sim, iters)
+        writer.add_scalar("val_loss_cycle_A", val_loss_cycle_A / val_size_sim, iters)
+        writer.add_scalar("val_loss_cycle_B", val_loss_cycle_B / val_size_sim, iters)
+
 
     # Visualizing Generated Images after each epoch
     with torch.no_grad():
